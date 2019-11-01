13f95f337138d8ed5e5c6d12cc47e64ec39e439d
keremet <keremet@solaris.kirov.ru>
2019-11-01 21:53:37 +0300

    Исправлена информация о лицензии

--- api/add_unit.php
+++ api/add_unit.php
@@ -1,9 +1,9 @@
 <?php
 /*
- * add_jp.php
  * This file is part of Satushem.
  *
- * Copyright (C) 2019
+ * Copyright (C) 2019 - Andrey Sokolov
+ * Copyright (C) 2019 - Michail Kudryavtsev
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
