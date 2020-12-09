00ecbe49ab584d4ce98598f450771ffc4956dfac
Andrey Sokolov <keremet@solaris.kirov.ru>
2020-12-09 22:21:39 +0300

    Реализовано дерево категорий

--- api/get_cat_tree.php
+++ api/get_cat_tree.php
@@ -20,11 +20,23 @@
  */
 include('headers.php');
 include('connect.php');
+
+function getChildren($id) {
+	global $db;
+	$stmtС = $db->prepare("SELECT id, name FROM category WHERE id_hi = ?");
+	$stmtС->execute(array($id));
+	$categ_arr = array();
+	while( $row = $stmtС->fetch() ) {
+		$categ_arr[] = array('id' => $row['id'], 'name' => $row['name'], 'children' => getChildren($row['id']) );
+	};
+	return $categ_arr;
+}
+
 $stmt = $db->prepare("SELECT id, name FROM category WHERE id_hi is null");
 $stmt->execute();
 $categ_arr = array();
 while( $row = $stmt->fetch() ) {
-	$categ_arr[] = array('id' => $row['id'], 'name' => $row['name'], 'children' => array());
+	$categ_arr[] = array('id' => $row['id'], 'name' => $row['name'], 'children' => getChildren($row['id']));
 };
 echo json_encode(
 	array('meta' => array('status' => 200, 'success' => true)
